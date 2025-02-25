f6bab6ed4b8f4e0d26b0c25e7403d35a91964714
gokulraman <36411817+gokulraman@users.noreply.github.com>
2025-02-25 10:57:36 +0530

    Update main.tf

--- Scripts/main.tf
+++ Scripts/main.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-05b10e08d247fb927"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
