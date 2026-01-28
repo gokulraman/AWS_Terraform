6bdc3822050a85c8946db6afc1f2754fb9ee4989
gokul <gokularaman27@gmail.com>
2026-01-28 14:15:41 +0530

    fix error

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,10 @@ source_suffix = {
 
 html_theme = "sphinx_book_theme"
 
-sphinx_external_needs={
+myst_enable_extensions = [
+    "colon_fence"
+]
+sphinx_external_needs=[
     dict(directive="req", title="Requirement", prefix="REQ_", color="#BFD8D2",style="node", style_data={"shape": "box"}),
     dict(directive="spec", title="Specification", prefix="SPEC_", color="#F5B7B1",style="node", style_data={"shape": "box3d"}),
-}
+]
